07e1ac8fe01836d892cb32d8dd332e8438bd3dce
James Stine <leon.blade@gmail.com>
2020-04-23 18:35:21 -0400

    Try to add Mono.Cecil for reflection!

--- CoreTestProject/Program.cs
+++ CoreTestProject/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Mono.Cecil;
+using System;
 
 namespace CoreTestProject
 {
 	class Program
 	{
-		static void Main(string[] args)
+		string testString = "See this field in reflection!";
+		int testNumber = 69;
+
+		static void Main(string[] _)
 		{
 			Console.WriteLine("Hello World!");
+
+			var path = @"./CoreTestProject.dll";
+
+			var module = ModuleDefinition.ReadModule(path);
+			var program = module.GetType("CoreTestProject.Program");
+
+			foreach (var field in program.Fields)
+			{
+				Console.WriteLine($"{field.Name}: {field.FieldType.Name}");
+			}
 		}
 	}
 }
